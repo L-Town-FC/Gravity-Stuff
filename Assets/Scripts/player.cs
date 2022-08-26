ebf72b9780fea4889b685d240702045673066e9f
ColinKrohto <7willythekid@gmail.com>
2022-08-26 12:00:21 -0400

    Organized code/added comments/added cooldown
    Moved code into functions and added comments to make it easier to figure out what its actually doing. Also added basic cooldown function for changing gravity

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,71 +6,56 @@ using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
-    //TODO: Play with accleration rate to make it seem less jarring when you switch gravity
-    //TODO: Implement cooldown for gravity changing
-
+    //Action maps and inputs
     private PlayerControls playerControls;
     private InputAction movement;
     private InputAction cameraMovement;
-    Transform playerCam;
+    Vector3 movementInput = Vector3.zero; //holds players inputs
+    Vector2 cameraInput = Vector2.zero; //holds players inputs
 
+    //player components
+    Transform playerCam;
     CapsuleCollider capsuleCollider;
     Rigidbody rb;
+
+    //camera variables
+    [SerializeField]
+    Vector2 cameraSensitivity = new Vector2(0.5f, 0.3f);
+    Vector2 minAndMaxCameraAngle = new Vector2(-75f, 75f);
+
+    //gravity variables
+    Vector3 up;
+    float gravityChangeCooldownTime = 2f;
+    float lastGravityChangeTime = 0f;
+    float acceleration = -0.75f; //acceleration due to gravity
+
     Vector3[] localLowerBounds; //holds points on player that are raycast from to check if grounded
     bool isGrounded = false;
     float verticalVelocity; //stores the players vertical velocity due to jumping/gravity
-    float acceleration = -1f; //acceleration due to gravity
     float jumpForce = 20f; //velocity magnitude that is set when player jumps
     float moveSpeed = 7f; //speed at which players moves
-    Vector3 movementInput = Vector3.zero; //holds players inputs
-    Vector2 cameraInput = Vector2.zero; //holds players inputs
-    [SerializeField]
-    Vector2 cameraSensitivity = new Vector2(0.5f, 0.3f);
-    Vector2 minAndMaxCameraAngle = new Vector2(-60f, 60f);
-    Vector3 up;
 
     private void Awake()
     {
-        playerControls = new PlayerControls();
-
-        playerCam = transform.GetChild(0);
-        capsuleCollider = GetComponent<CapsuleCollider>();
-        rb = GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
-        localLowerBounds = GetLowerBounds(capsuleCollider.center, capsuleCollider.radius);
-        up = transform.up;
+        SettingInitialPlayerConditions();
     }
 
     private void OnEnable()
     {
         //enabling the different player controls
-        movement = playerControls.PlayerMovement.Walking;
-        movement.Enable();
-        cameraMovement = playerControls.PlayerMovement.Camera;
-        cameraMovement.Enable();
-
-
-        playerControls.PlayerMovement.GravityChange.performed += ChangeGravity;
-        playerControls.PlayerMovement.GravityChange.Enable();
-
-        playerControls.PlayerMovement.Jump.performed += DoJump;
-        playerControls.PlayerMovement.Jump.Enable();
+        SettingPlayerControls();
     }
     //grab inputs in update
     private void Update()
     {
-        movementInput = new Vector3(movement.ReadValue<Vector2>().x, 0f, movement.ReadValue<Vector2>().y);
-        cameraInput = new Vector2(cameraMovement.ReadValue<Vector2>().x, cameraMovement.ReadValue<Vector2>().y);
-        isGrounded = GroundCheck(localLowerBounds, capsuleCollider.radius, -transform.up, transform);
+        GettingPlayerInputs();
         Debug.DrawRay(transform.position, up * 10f);
         Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
-
     }
 
     //apply updates in fixed update
     private void FixedUpdate()
     {
-        //SettingNewGravity();
         if (!isGrounded) //if the player is grounded reset their downward motion so it doesnt constantly build up. if they are not grounded, begin applying gravity
         {
             verticalVelocity = Gravity(verticalVelocity, acceleration);
@@ -86,10 +71,7 @@ public class player : MonoBehaviour
     private void OnDisable()
     {
         //need to disable incase object is destroyed and game tries to call them after
-        movement.Disable();
-        cameraMovement.Disable();
-        playerControls.PlayerMovement.Jump.Disable();
-        playerControls.PlayerMovement.GravityChange.Disable();
+        DisablingPlayerControls();
     }
 
     private void DoJump(InputAction.CallbackContext obj)
@@ -104,31 +86,37 @@ public class player : MonoBehaviour
     {
         if (obj.performed)
         {
-            Vector3 newGravity = new Vector3(obj.ReadValue<Vector2>().x, 0f, obj.ReadValue<Vector2>().y);
-            if (!(newGravity.x == 0.71f || newGravity.x == -0.71f || newGravity.z == 0.71f || newGravity.z == -0.71f)){
-                //transform newGRavity so its relative to player
-                //only take largest component of newgravity and set others to zero
-                //normalize and set to up
-                newGravity = transform.TransformDirection(newGravity);
+            Vector3 newGravity = new Vector3(obj.ReadValue<Vector2>().x, 0f, obj.ReadValue<Vector2>().y); //grabbing inputs
 
-                Vector3 newUp = new Vector3(Mathf.RoundToInt(newGravity.x), 0f, 0f);
-                float max = Mathf.Abs(newGravity.x);
-
-                if(Mathf.Abs(newGravity.y) > max)
-                {
-                    max = Mathf.Abs(newGravity.y);
-                    newUp = new Vector3(0f, Mathf.RoundToInt(newGravity.y), 0f);
-                }
-
-                if (Mathf.Abs(newGravity.z) > max)
-                {
-                    newUp = new Vector3(0f, 0f, Mathf.RoundToInt(newGravity.z));
-                }
-                up = -newUp;
-                StartCoroutine("SettingGravity");
+            if ((newGravity.x == 0.71f || newGravity.x == -0.71f || newGravity.z == 0.71f || newGravity.z == -0.71f)){ //this makes sure that the player didnt hit d-pad diagonally. only up, down, left, or right
+                return;
             }
+            if(Time.time < lastGravityChangeTime + gravityChangeCooldownTime) //checks when the player last changed their gravity and blocks them from changing again until cooldown is done
+            {
+                return;
+            }
+
+            //checks the direction the player pressed on the d-pad and compares it world vectors and then sets the players up values equal to world vector that is closest to the input direction
+            newGravity = transform.TransformDirection(newGravity);
+
+            Vector3 newUp = new Vector3(Mathf.RoundToInt(newGravity.x), 0f, 0f);
+            float max = Mathf.Abs(newGravity.x);
+
+            if (Mathf.Abs(newGravity.y) > max)
+            {
+                max = Mathf.Abs(newGravity.y);
+                newUp = new Vector3(0f, Mathf.RoundToInt(newGravity.y), 0f);
+            }
+
+            if (Mathf.Abs(newGravity.z) > max)
+            {
+                newUp = new Vector3(0f, 0f, Mathf.RoundToInt(newGravity.z));
+            }
+            up = -newUp;
+            lastGravityChangeTime = Time.time;
+            StartCoroutine("SettingGravity");
         }
-        //checks the direction the player pressed on the d-pad and compares it world vectors and then sets the players up values equal to world vector that is closest to the input direction
+        
     }
 
 
@@ -197,23 +185,68 @@ public class player : MonoBehaviour
         return false;
     }
 
+    void SettingInitialPlayerConditions()
+    {
+        playerControls = new PlayerControls();
+
+        playerCam = transform.GetChild(0);
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        localLowerBounds = GetLowerBounds(capsuleCollider.center, capsuleCollider.radius);
+        up = transform.up;
+    }
+
+    void SettingPlayerControls()
+    {
+        movement = playerControls.PlayerMovement.Walking;
+        movement.Enable();
+        cameraMovement = playerControls.PlayerMovement.Camera;
+        cameraMovement.Enable();
+
+
+        playerControls.PlayerMovement.GravityChange.performed += ChangeGravity;
+        playerControls.PlayerMovement.GravityChange.Enable();
+
+        playerControls.PlayerMovement.Jump.performed += DoJump;
+        playerControls.PlayerMovement.Jump.Enable();
+    }
+
+    void GettingPlayerInputs()
+    {
+        movementInput = new Vector3(movement.ReadValue<Vector2>().x, 0f, movement.ReadValue<Vector2>().y);
+        cameraInput = new Vector2(cameraMovement.ReadValue<Vector2>().x, cameraMovement.ReadValue<Vector2>().y);
+        isGrounded = GroundCheck(localLowerBounds, capsuleCollider.radius, -transform.up, transform);
+    }
+    void DisablingPlayerControls()
+    {
+        movement.Disable();
+        cameraMovement.Disable();
+        playerControls.PlayerMovement.Jump.Disable();
+        playerControls.PlayerMovement.GravityChange.Disable();
+    }
+
     IEnumerator SettingGravity()
     {
+        //changes the players up direction to its newly set one
         bool isFinished = false;
         Vector3 finalDirection = transform.position + transform.TransformDirection(Vector3.forward);
         while (!isFinished)
         {
-            if(Vector3.Dot(transform.up, up) >= 0.99999f)
+            if(Vector3.Dot(transform.up, up) >= 0.99999f) //checks if the players current up is close to its target up
             {
+                //whens its close it just manually sets it to be exact
                 transform.rotation.SetLookRotation(finalDirection, up);
+
+                //rounds the players euler angles to intergers. without this the player may be tilted by a few degrees which would be jarring/look bad
                 Vector3 fineAdjustment = transform.localEulerAngles;
                 float x = Mathf.RoundToInt(fineAdjustment.x);
                 float y = Mathf.RoundToInt(fineAdjustment.y);
                 float z = Mathf.RoundToInt(fineAdjustment.z);
                 transform.localEulerAngles = new Vector3(x, y, z);
-                print(transform.localEulerAngles);
                 isFinished = true;
             }
+            //generates new rotation for player towards their new up
             Quaternion slopeRotation = Quaternion.FromToRotation(transform.up, up);
 
             transform.rotation = Quaternion.Lerp(transform.rotation, slopeRotation * transform.rotation, 15f * Time.deltaTime);
